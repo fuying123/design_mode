3f95a1324010eb2265cdb00d4a634db9dd0c66e4
fuying <ying.fu@lovdream.com>
2021-02-25 17:08:01 +0800

    rm cit

--- cit/FactoryKit/src/com/lovdream/factorykit/FragmentKeyHandler.java
+++
@@ -1,9 +0,0 @@
-
-package com.lovdream.factorykit;
-
-import android.view.KeyEvent;
-
-public interface FragmentKeyHandler{
-	public boolean onKeyDown(int keyCode,KeyEvent event);
-	public boolean onKeyUp(int keyCode,KeyEvent event);
-}
--- cit/FactoryKit/src/com/lovdream/factorykit/FragmentNewIntentHandler.java
+++
@@ -1,7 +0,0 @@
-package com.lovdream.factorykit;
-
-import android.content.Intent;
-
-public interface FragmentNewIntentHandler{
-	public void onNewIntent(Intent intent);
-}
